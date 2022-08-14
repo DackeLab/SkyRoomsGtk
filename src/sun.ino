2ac885c38f223aa61c6a593660ea2afab536e738
Yakir Gagnon <12.yakir@gmail.com>
2022-08-14 14:32:57 +0200

    update led arduino

--- src/sun.ino
+++ src/sun.ino
@@ -3,13 +3,17 @@
 #define NUM_LEDS 300
 #define DATA_PIN 4
 #define CLOCK_PIN 5
-#define NUM_SUNS 10
+#define NUM_SUNS 80
+
+uint8_t leds_id = 255; //sheldon: 255, nicolas:254
 
 CRGB leds[NUM_LEDS];
 
 uint16_t suns[NUM_SUNS][2];
 
 void setup() { 
+  pinMode(LED_BUILTIN, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
   Serial.begin(115200);
   FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
   FastLED.clear();
@@ -25,6 +29,10 @@ void loop() {
   uint8_t nbytes = Serial.readBytes(buff, 7);
   if(nbytes == 7) {
     uint8_t i = buff[0];
+    if (i == 255){
+      Serial.write(leds_id);
+    }
+    else {
     fill_solid( &(leds[suns[i][0]]), suns[i][1], CRGB::Black);
     
     uint16_t start = (buff[2] << 8) + buff[1];
@@ -35,5 +43,6 @@ void loop() {
     suns[i][1] = len;
     
     FastLED.show();
+    }
   }
 }
