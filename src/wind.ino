a52333f89f8cf240a49534ea4ab0b742ddbe3f7f
Yakir Gagnon <12.yakir@gmail.com>
2022-08-16 13:32:12 +0200

    wind.ino doc

--- src/wind.ino
+++ src/wind.ino
@@ -1,7 +1,7 @@
-#include <TimerOne.h>
+#include <TimerOne.h> // needed to create PWM at 25 kHz
 
 #define PIN_PWM 9
-uint8_t fanid = 3;
+uint8_t fanid = 3; // each fan-group has a unique ID number
 uint8_t duty = 0;
 
 void setup(void)
@@ -10,21 +10,22 @@ void setup(void)
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off
   Timer1.initialize(40);  // 40 us = 25 kHz
   Serial.begin(115200);
-  Timer1.pwm(PIN_PWM, 0);
+  Timer1.pwm(PIN_PWM, 0); // kill the fans
 }
 
 void loop(void)
 {
   if (Serial.available() > 0) {
-    uint8_t input = Serial.read();
-    if (input == 255) {
+    uint8_t input = Serial.read(); // this will either be the duty the fan should be set at, or it'll be equal to 255
+    if (input == 255) { // if it's 255 then the computer wants to know what this fan's ID number is
       Serial.write(fanid);
     }
-    else {
-      noInterrupts();
+    else { // if it's <255 then it's the duty
+      noInterrupts(); // I think we need this to successfully copy the input
       duty = input;
       interrupts();
     }
   }
+  // The maximum duty we can set is 1023. The minimum value that moves the fans is 55 (just by experimentation). Input is a byte so it can only be 0--255. We use 255 as a flag to get the fan's ID. So that leaves us with 0--254. Therefore, the maximum value we have is 254, which explains the following calculation (I didn't normalize for the low bound of 55, cause I think it's ok that we have a dead safe zone at the bottom):
   Timer1.pwm(PIN_PWM, (float)duty / 254.0 * 1023.0); // from 55 to 1023
 }
