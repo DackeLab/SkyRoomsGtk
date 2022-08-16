8779e6dedfd3cfe1a0877baf3dbd4b1defe7da5c
yakir12 <12.yakir@gmail.com>
2022-08-16 18:19:57 +0200

    doc

--- src/arduino.jl
+++ src/arduino.jl
@@ -1,24 +1,25 @@
+# try to identify if a port is connected to an Arduino, and if so, which one is it
 function identify_arduino(port)
     sp = open(port, baudrate)
     sleep(1)
-    set_flow_control(sp)
+    set_flow_control(sp) # necessary for macs
     sleep(1)
     bytes = UInt8[]
-    for _ in 1:10
+    for _ in 1:10 # 10 attempts because communication is very brittle
         sleep(0.1)
-        write(sp, UInt8[255, 0, 0, 0, 0, 0, 0])
+        write(sp, UInt8[255, 0, 0, 0, 0, 0, 0]) # 255 signals to the arduino that we want its ID
         append!(bytes, read(sp))
-        if !isempty(bytes)
-            break
+        if !isempty(bytes) # ah! We got a response
+            break 
         end
     end
     id = Int(only(bytes))
-    if id > 128
-        suns_arduino[] = sp
-        cardinalities[] = id == 255 ? ["NE", "SW", "SE", "NW"] : ["SE", "NW", "NE", "SW"] 
+    if id > 128 # LED strips have high ID numbers
+        suns_arduino[] = sp # stow this serial port
+        cardinalities[] = id == 255 ? ["NE", "SW", "SE", "NW"] : ["SE", "NW", "NE", "SW"] # due to differences in how the strips are wired we have some discrepancies in their cardinality. The first one is Sheldon, the second is Nicolas
         @info "found LED arduino"
     else
-        winds_arduinos[][id] = sp
+        winds_arduinos[][id] = sp # stow the fans' serial port
         @info "found wind $id arduino"
     end
 end
@@ -27,7 +28,7 @@ function populate_arduinos()
     ntries = 5
     for port in get_port_list()
         for i in 1:ntries
-            try
+            try # when opening "bad" ports (blue tooth modules etc) this will error, so  wrapped it in a try/catch thing
                 identify_arduino(port)
                 break
             catch ex
