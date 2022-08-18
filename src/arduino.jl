f46d2f87c230e10a45941b138a8818416dbbed44
yakir12 <12.yakir@gmail.com>
2022-08-18 13:07:40 +0200

    better serial com

--- src/arduino.jl
+++ src/arduino.jl
@@ -1,19 +1,21 @@
+function get_id(sp)
+    ntries = 5
+    for _ in 1:ntries # `ntries` attempts because communication is very brittle
+        write(sp, UInt8[255, 0, 0, 0, 0, 0, 0]) # 255 signals to the arduino that we want its ID
+        if bytesavailable(sp) > 0
+            return only(read(sp))
+        end
+        sleep(0.1)
+    end
+    error("connected device did not return any ID")
+end
 # try to identify if a port is connected to an Arduino, and if so, which one is it
 function identify_arduino(port)
     sp = open(port, baudrate)
-    sleep(1)
-    set_flow_control(sp) # necessary for macs
-    sleep(1)
-    bytes = UInt8[]
-    for _ in 1:10 # 10 attempts because communication is very brittle
-        sleep(0.1)
-        write(sp, UInt8[255, 0, 0, 0, 0, 0, 0]) # 255 signals to the arduino that we want its ID
-        append!(bytes, read(sp))
-        if !isempty(bytes) # ah! We got a response
-            break 
-        end
-    end
-    id = Int(only(bytes))
+    sleep(0.1)
+    set_flow_control(sp; rts=SP_RTS_ON, dtr=SP_DTR_ON) # necessary for macs
+    sleep(0.1)
+    id = get_id(sp)
     if id > 128 # LED strips have high ID numbers
         suns_arduino[] = sp # stow this serial port
         cardinalities[] = id == 255 ? ["NE", "SW", "SE", "NW"] : ["SE", "NW", "NE", "SW"] # due to differences in how the strips are wired we have some discrepancies in their cardinality. The first one is Sheldon, the second is Nicolas
